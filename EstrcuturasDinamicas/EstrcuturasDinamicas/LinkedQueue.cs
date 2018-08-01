d45cf6683226dd46ed22be72f181e696feea5be3
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2018-07-31 21:44:11 -0600

    corrección

---
+++ EstrcuturasDinamicas/EstrcuturasDinamicas/LinkedQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstrcuturasDinamicas
+{
+    class LinkedQueue<T> : Queue<T>
+    {
+        LinkedList<T> myList;
+        public LinkedQueue()
+        {
+            myList = new LinkedList<T>();
+        }
+
+        public override T dequeue()
+        {
+            return myList.removeFirst();
+        }
+
+        public override T front()
+        {
+            return myList.first();
+        }
+
+        public override bool isEmpty()
+        {
+            return myList.isEmpty();
+        }
+
+        public override void enqueue(T t)
+        {
+            myList.addLast(t);
+        }
+
+        public override int size()
+        {
+            return myList.size();
+        }
+    }
+}
---
+++ EstrcuturasDinamicas/EstrcuturasDinamicas/Stack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstrcuturasDinamicas
+{
+    abstract class Stack<T>
+    {
+        public abstract int size();
+        public abstract bool isEmpty();
+        public abstract void push(T t);
+        public abstract T pop();
+        public abstract T top();
+    }
+}
