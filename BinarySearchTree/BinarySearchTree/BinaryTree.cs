4e75a29cfebcea9c711a106bee910f487c453afa
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2018-08-05 21:02:45 -0600

    Agregación método insertar, evitando duplicados

---
+++ BinarySearchTree/BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree<T>
+    {
+        Node<T> root;
+        private int treeSize = 0;
+        Comparer<T> comp = Comparer<T>.Default;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public int size()
+        {
+            return treeSize;
+        }
+
+        public void add(Node<T> root, T element)
+        {            
+            if (root == null)
+            {
+                root = new Node<T>(element, null, null); ;
+            }
+            else
+            {                
+                if (comp.Compare(element, root.getElement()) < 0)//x es menor que y
+                {
+                    if (root.getLeft() == null)
+                    {
+                        root.setLeft(new Node<T>(element, null, null));
+                    }
+                    else
+                    {
+                        add(root.getLeft(), element);
+                    }
+                }
+                else if (comp.Compare(element, root.getElement()) > 0)//x es mayor que y 
+                {
+                    if (root.getRight() == null)
+                    {
+                        root.setRight(new Node<T>(element, null, null));
+                    }
+                    else
+                    {
+                        add(root.getRight(), element);
+                    }
+                }
+                else//x es igual que y
+                {
+                    throw new Exception("NO SE PERMITEN DUPLICADOS");
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node<T>
+    {
+        private T element;        
+        private Node<T> left;
+        private Node<T> right; 
+        
+        public Node(T t, Node<T> leftChild, Node<T> rightChild)
+        {
+            element = t;            
+            left = leftChild;
+            right = rightChild;
+        }
+
+        public T getElement() 
+        {
+			return element;
+        }      
+
+        public Node<T> getLeft()
+        {
+            return left;
+        }
+
+        public Node<T> getRight()
+        {
+            return right;
+        }
+
+        public void setElement(T t)
+        {
+            element = t;
+        }
+
+        public void setLeft(Node<T> leftChild)
+        {
+            left = leftChild;
+        }
+
+        public void setRight(Node<T> rightChild)
+        {
+            right = rightChild;
+        }
+    }
+}
