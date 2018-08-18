5b46654407b1b9d2a7b060c8c8823bc2845a9198
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2018-08-17 20:58:38 -0600

    Implementación lista, pila y cola.

--- DSImplementation/DSImplementation/Program.cs
+++ DSImplementation/DSImplementation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EstrcuturasDinamicas;
 
 namespace DSImplementation
 {
@@ -10,6 +11,52 @@ namespace DSImplementation
     {
         static void Main(string[] args)
         {
+            MyLinkedList<int> myList = new MyLinkedList<int>();
+            LinkedStack<int> myStack = new LinkedStack<int>();
+            LinkedQueue<int> myQueue = new LinkedQueue<int>();
+            int cont = 0;
+            myList.addLast(1);
+            myList.addLast(3);
+            myList.addLast(7);
+            myList.addLast(5);
+            myList.addFirst(10);
+            cont = myList.size();
+
+            Console.WriteLine("LISTA");
+            Console.WriteLine("El primer elemento de la lista es: " + myList.first());
+            Console.WriteLine("El ultimo elemento de la lista es: " + myList.last());
+            for (int i = 0; i < cont; i++)
+            {
+                Console.WriteLine(myList.removeFirst());
+            }
+            Console.WriteLine("\nEl primer elemento de la lista es: " + myList.first());
+            Console.WriteLine("El ultimo elemento de la lista es: " + myList.last());
+            Console.WriteLine("El nuevo tamaño de la lista es: " + myList.size());
+
+            myStack.push(1);
+            myStack.push(3);
+            myStack.push(7);
+            myStack.push(5);
+            myStack.push(10);
+            Console.WriteLine("\nPILA");
+            Console.WriteLine("El primer elemento de la pila es: " + myStack.top());
+            for (int i = 0; i < cont; i++)
+            {
+                Console.WriteLine(myStack.pop());
+            }
+
+            myQueue.enqueue(1);
+            myQueue.enqueue(3);
+            myQueue.enqueue(7);
+            myQueue.enqueue(5);
+            myQueue.enqueue(10);
+            Console.WriteLine("\nCOLA");
+            Console.WriteLine("El primer elemento de la pila es: " + myQueue.front());
+            for (int i = 0; i < cont; i++)
+            {
+                Console.WriteLine(myQueue.dequeue());
+            }            
+            Console.ReadLine();
         }
     }
 }
