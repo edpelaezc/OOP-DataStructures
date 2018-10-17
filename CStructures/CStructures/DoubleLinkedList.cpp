ae8c58008308d9f585f66f8ecd38f433a60f179e
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2018-10-16 22:15:55 -0600

    Lista doblemente enlazada terminada.

---
+++ CStructures/CStructures/DoubleLinkedList.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+#include "DoubleLinkedList.h"
+#include <iostream>
+
+DoubleLinkedList::DoubleLinkedList()
+{
+	header = new Node(NULL, NULL, NULL);
+	trailer = new Node(NULL, header, NULL);
+	header->setNext(trailer);
+	listSize = 0;
+}
+
+int DoubleLinkedList::size() { return listSize; }
+
+bool DoubleLinkedList::isEmpty() { return listSize == 0; }
+
+int DoubleLinkedList::first() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return header->getNext()->getElement();
+	}
+}
+
+int DoubleLinkedList::last() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return trailer->getPrev()->getElement();
+	}
+}
+
+void DoubleLinkedList::addFirst(int n) {
+	addBetween(n, header, header->getNext());
+}
+
+void DoubleLinkedList::addLast(int n) {
+	addBetween(n, trailer->getPrev(), trailer);
+}
+
+int DoubleLinkedList::removeFirst() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return remove(header->getNext());
+	}
+}
+
+int DoubleLinkedList::removeLast() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return remove(trailer->getPrev());
+	}
+}
+
+void DoubleLinkedList::addBetween(int n, node predecessor, node successor) {
+	node newest = new Node(n, predecessor, successor);
+	predecessor->setNext(newest);
+	successor->setPrev(newest);
+	listSize++;
+}
+
+int DoubleLinkedList::remove(Node *node) {
+	Node *predecessor = node->getPrev();
+	Node *successor = node->getNext();
+	predecessor->setNext(successor);
+	successor->setPrev(predecessor);
+	listSize--;
+	return node->getElement();
+}
+
+DoubleLinkedList::~DoubleLinkedList()
+{
+}
---
+++ CStructures/CStructures/DoubleLinkedList.h
@@ -0,0 +1,58 @@
+#pragma once
+
+class Node {
+private:
+	int element;
+	Node *prev;
+	Node *next;
+
+public:
+	Node(int e, Node *previousNode, Node *nextNode) {
+		element = e;
+		prev = previousNode;
+		next = nextNode;
+	}
+
+	int getElement() {
+		return element;
+	}
+
+	Node *getPrev() {
+		return prev;
+	}
+
+	void setPrev(Node *previous) {
+		prev = previous;
+	}
+
+	Node *getNext() {
+		return next;
+	}
+
+	void setNext(Node *nextNode) {
+		next = nextNode;
+	}
+}; typedef Node *node;
+
+
+class DoubleLinkedList
+{
+private:
+	node header;
+	node trailer;
+	int listSize;
+	void addBetween(int n, node predecessor, node successor);
+	int remove(Node *node);
+public:	
+	DoubleLinkedList();
+	int size();
+	bool isEmpty();
+	int first();
+	int last();
+	void addFirst(int n);
+	void addLast(int n);
+	int removeFirst();
+	int removeLast();
+	~DoubleLinkedList();
+};
+
