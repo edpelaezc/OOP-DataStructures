e45b84a7128d67b293dab0c84d29abce652a0733
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2018-10-28 18:59:55 -0600

    Métodos implementados.

--- GenericStructures/GenericStructures/MyLinkedList.cpp
+++ GenericStructures/GenericStructures/MyLinkedList.cpp
@@ -16,26 +16,88 @@ template<class Data>
 bool MyLinkedList<Data>::isEmpty() { return listSize == 0; }
 
 template<class Data>
-Data MyLinkedList<Data>::first() {}
+Data MyLinkedList<Data>::first() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return head->getElement();
+	}
+}
 
 template<class Data>
-Data MyLinkedList<Data>::last() {}
+Data MyLinkedList<Data>::last() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		return tail->getElement();
+	}
+}
 
 
 template<class Data>
-void MyLinkedList<Data>::addFirst(Data d) {}
+void MyLinkedList<Data>::addFirst(Data d) {
+	head = new Node<Data>(d, head);
+	if (listSize == 0) {
+		tail = head;
+	}
+	listSize++;
+}
 
 template<class Data>
-void MyLinkedList<Data>::addLast(Data d) {}
+void MyLinkedList<Data>::addLast(Data d) {
+	Node<Data> newest = new Node<Data>(d, NULL);
+	if (isEmpty()) {
+		head = newest;
+	}
+	else {
+		tail->setNext(newest);
+	}
+	tail = newest;
+	listSize++;
+}
 
 template<class Data>
-Data MyLinkedList<Data>::removeFirst() {}
+Data MyLinkedList<Data>::removeFirst() {
+	if (isEmpty()) {
+		return NULL;
+	}
+	else {
+		Data auxiliary = head->getElement();
+		head = head->getNext();
+		listSize--;
+		if (listSize == 0)
+		{
+			tail = NULL;
+		}
+		return auxiliary;
+	}
+}
 
 template<class Data>
-bool MyLinkedList<Data>::searchElement(Data reference) {}
+bool MyLinkedList<Data>::searchElement(Data reference) {
+	Node<Data> auxiliar = head;
+	bool flag = true;
+	while (auxiliar != NULL && flag) {
+		if (auxiliar->getElement() == reference) {
+			flag = false;
+		}
+		else {
+			auxiliar = auxiliar->getNext();
+		}
+	}
+	return !flag;
+}
 
 template<class Data>
-void MyLinkedList<Data>::showElements() {}
+void MyLinkedList<Data>::showElements() {
+	Node<Data> auxiliar = head;
+	while (auxiliar != NULL) {
+		std::cout << auxiliar->getElement() << "\t";
+		auxiliar = auxiliar->getNext();
+	}
+}
 
 template<class Data>
 MyLinkedList<Data>::~MyLinkedList() {
