45bf005785e30f524132dca90d70423ba021fa1b
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2019-08-16 14:16:11 -0600

    ejercicio

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultado = 0;
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a > 0 || b > 0) //los dos son positivos
+            {
+                a *= -1;
+                resultado = a - b;
+                resultado *= -1;
+                Console.WriteLine("Resultado: " + resultado);
+            }
+            else if (a < 0 && b < 0) // los dos son negativos
+            {
+                a *= -1;
+                resultado = b - a;                
+                Console.WriteLine("Resultado: " + resultado);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
